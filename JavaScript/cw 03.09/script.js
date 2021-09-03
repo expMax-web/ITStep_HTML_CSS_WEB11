28524828da56790b25634eabe329040fa6ffffee
expMax-web <84277100+expMax-web@users.noreply.github.com>
2021-09-03 20:45:50 +0400

    cw 03.09

---
+++ JavaScript/cw 03.09/script.js
@@ -0,0 +1,41 @@
+// const clicker = document.querySelector("#clicker")
+
+// clicker.onclick = () => {
+//     alert("Нажал!");
+// };
+
+// const clicker = document.querySelector("#clicker")
+
+// clicker.addEventListener("click", () => {alert("Нажал!")});
+
+// clicker.addEventListener("click", () => {console.log("НАажал");});
+
+/*Задание 1
+const generateNum = document.querySelector("#createNumButton");
+
+generateNum.addEventListener("click", () => {
+  const randomNumberNode = document.querySelector("#randomNumber");
+  const randomNum = Math.floor(Math.random(100) * 100);
+  randomNumberNode.innerHTML = randomNum;
+});*/
+
+//Задание 2
+/*
+const app = document.getElementById("app");
+
+app.addEventListener("mousemove", (event) => {
+  app.innerHTML = `X: ${event.clientX}, Y: ${event.clientY}`;
+});
+
+app.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+});
+
+const BUTTON = ["ЛКМ", "СКМ", "ПКМ"];
+
+app.addEventListener("mouseup", (event) => {
+  app.innerText = BUTTON[event.button];
+});
+*/
+
+//Задание 4
