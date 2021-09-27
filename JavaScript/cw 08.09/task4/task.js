e7547554e8277cf51d495dfc7db5e97fd1c7e90c
expMax-web <84277100+expMax-web@users.noreply.github.com>
2021-09-27 20:46:13 +0400

    123

--- JavaScript/cw 08.09/task4/task.js
+++ JavaScript/cw 08.09/task4/task.js
@@ -1,6 +1,20 @@
 const paletteContainer = document.getElementById("paletteContainer");
 const textContainer = document.getElementById("text");
 
+const pallete = [];
+let str = "";
+while (pallete.length < 5) {
+  pallete.push(
+    `rgb (${Math.floor(Math.random() * 257)}, ${Math.floor(
+      Math.random() * 257
+    )}, ${Math.floor(Math.random() * 257)})`
+  );
+}
+
+const buttonListGtml = pallete.map((color) => {
+  `<button>`;
+});
+
 paletteContainer.addEventListener("click", (event) => {
   console.log(event.target.dataset.color);
   textContainer.style = `color:${event.target.dataset.color}`;
